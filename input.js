48b730f017d286b28c40e7976039641b4652b473
Rajan Lail <rajanlail3993@gmail.com>
2023-06-13 16:36:16 -0700

    Add setupInput and handleUserInput. From play.js

---
+++ input.js
@@ -0,0 +1,19 @@
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (data) {
+  if (data === '\u0003') {
+    // Check if Ctrl + C pressed
+    process.exit(); // Terminate the process
+  }
+};
+
+module.exports = { setupInput };
