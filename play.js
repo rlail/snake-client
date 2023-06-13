0a49b380ee9168a4dc495429d881fc0031b5ff2e
Rajan Lail <rajanlail3993@gmail.com>
2023-06-13 16:35:42 -0700

    Remove functions. Add module.exports

--- play.js
+++ play.js
@@ -1,25 +1,8 @@
 const net = require("net");
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect(net);
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (data) {
-  if (data === '\u0003') {
-    // Check if Ctrl + C pressed
-    process.exit(); // Terminate the process
-  }
-};
-
-setupInput(); // Call the setupInput function 
+setupInput();
