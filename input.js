389448e049a1b2c7503dc003bf706b11bda15f8f
Rajan Lail <rajanlail3993@gmail.com>
2023-06-13 18:26:42 -0700

    Update setupInput and handleUserInput

--- input.js
+++ input.js
@@ -1,6 +1,7 @@
-// setup interface to handle user input from stdin
+let connection;
 
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -13,6 +14,14 @@ const handleUserInput = function (data) {
   if (data === '\u0003') {
     // Check if Ctrl + C pressed
     process.exit(); // Terminate the process
+  } else if (data === 'w') { //WASD inputs
+    connection.write("Move: up");
+  } else if (data === 'a') {
+    connection.write("Move: left");
+  } else if (data === 's') {
+    connection.write("Move: down");
+  } else if (data === 'd') {
+    connection.write("Move: right");
   }
 };
 
